9325560226321502709973e226aa89a40d558dd9
James Njenga <knjenga94@gmail.com>
2022-10-29 23:45:11 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES 
+("Mr. Chcolate",24,"M","black","decisive",1),
+("Rowdy",18,"F","black","cool",1),
+("Tabitha",26,"F","white","chilled",1),
+("Seargent Brown",30,"M","black","cold",0),
+("Melissa",22,"F","white","grumpy",1),
+("Grinch",21,"M","black","hyper",1),
+("Wendy",20,"F","light skinned","cool",1),
+(NULL,27,"M","white","racist",0);
